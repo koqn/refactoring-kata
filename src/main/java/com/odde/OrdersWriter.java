ef8a3f5156cfb716395fbdf611f110b4af3801e5
koqn <koqn>
2018-02-17 19:08:34 +0900

    add getSingleOrderContents method

--- src/main/java/com/odde/OrdersWriter.java
+++ src/main/java/com/odde/OrdersWriter.java
@@ -7,25 +7,29 @@ public class OrdersWriter {
         this.orders = orders;
     }
 
+    public String getSingleOrderContents(Order order) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("{");
+        sb.append("\"id\": ");
+        sb.append(order.getOrderId());
+        sb.append(", ");
+        sb.append("\"products\": [");
+        for (int j = 0; j < order.getProductsCount(); j++) {
+            Product product = order.getProduct(j);
+            sb.append(product.getProductContents());
+            if (j !=  order.getProductsCount() - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]}");
+        return sb.toString();
+    }
+
     public String getContents() {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-                sb.append(product.getProductContents());
-                if (j !=  order.getProductsCount() - 1) {
-                    sb.append(", ");
-                }
-            }
-
-            sb.append("]}");
+            sb.append(getSingleOrderContents(orders.getOrder(i)));
             if (i != orders.getOrdersCount() - 1) {
                 sb.append(", ");
             }
